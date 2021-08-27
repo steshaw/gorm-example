1982fc110dd96c8146834cd1ea0fedb3f5e6ab68
Steven Shaw <steven@steshaw.org>
2021-08-27 12:14:41 +1000

    Print

--- main.go
+++ main.go
@@ -3,6 +3,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -27,14 +29,19 @@ func main() {
 
 	// Read
 	var product Product
-	db.First(&product, 1)                 // find product with integer primary key
+	db.First(&product, 1) // find product with integer primary key
+	fmt.Printf("Found with id=1: %+v\n", product)
 	db.First(&product, "code = ?", "D42") // find product with code D42
+	fmt.Printf("Found with code=D42: %+v\n", product)
 
 	// Update - update product's price to 200
 	db.Model(&product).Update("Price", 200)
+	fmt.Printf("Updated price to 200: %+v\n", product)
 	// Update - update multiple fields
 	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
+	fmt.Printf("Updated Price to 200 and Code to F42 method 1: %+v\n", product)
 	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+	fmt.Printf("Updated Price to 200 and Code to F42 method 2: %+v\n", product)
 
 	// Delete - delete product
 	db.Delete(&product, 1)
