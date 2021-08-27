329916dfe00fbab4b69f37c1aa30ff3300c41af3
Steven Shaw <steven@steshaw.org>
2021-08-27 12:35:47 +1000

    Pretty printing and detect not found

--- go.mod
+++ go.mod
@@ -10,5 +10,8 @@ require (
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.1 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.5 // indirect
+	github.com/rogpeppe/go-internal v1.6.1 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/kr/pretty"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -27,22 +28,27 @@ func main() {
 	// Create
 	db.Create(&Product{Code: "D42", Price: 100})
 
-	// Read
 	var product Product
-	db.First(&product, 1) // find product with integer primary key
-	fmt.Printf("Found with id=1: %+v\n", product)
+
+	// Read
+	if err := db.First(&product, 1).Error; err != nil { // find product with integer primary key
+		fmt.Printf("Product with id=1 is not found\n")
+	} else {
+		fmt.Printf("Found with id=1: %# v\n", pretty.Formatter(product))
+	}
 	db.First(&product, "code = ?", "D42") // find product with code D42
-	fmt.Printf("Found with code=D42: %+v\n", product)
+	fmt.Printf("Found with code=D42: %# v\n", pretty.Formatter(product))
 
 	// Update - update product's price to 200
 	db.Model(&product).Update("Price", 200)
-	fmt.Printf("Updated price to 200: %+v\n", product)
+	fmt.Printf("Updated price to 200: %# v\n", pretty.Formatter(product))
 	// Update - update multiple fields
 	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
-	fmt.Printf("Updated Price to 200 and Code to F42 method 1: %+v\n", product)
+	fmt.Printf("Updated Price to 200 and Code to F42 method 1: %# v\n", pretty.Formatter(product))
 	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
-	fmt.Printf("Updated Price to 200 and Code to F42 method 2: %+v\n", product)
+	fmt.Printf("Updated Price to 200 and Code to F42 method 2: %# v\n", pretty.Formatter(product))
 
 	// Delete - delete product
 	db.Delete(&product, 1)
+	fmt.Printf("Deleted id 1, product= %# v\n", pretty.Formatter(product))
 }
